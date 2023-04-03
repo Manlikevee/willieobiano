3816914dcbb396ee401b75f00811766d4fec5355
ab <cruxCodes@gmail.com>
2023-04-03 17:24:40 +0100

    worked on the form

--- src/components/notableAchievement/NotableAchievement.jsx
+++ src/components/notableAchievement/NotableAchievement.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import image from "./images/Rectangle 92.png";
-import image1 from "./images/Rectangle 94.png";
-import image2 from "./images/Rectangle 93.png";
-import image3 from "./images/Rectangle 95.png";
-import image4 from "./images/Rectangle 96.png";
-import image5 from "./images/Rectangle 97.png";
-import image6 from "./images/Rectangle 98.png";
+
 import LayoutLight from "../../component/Layoutlight/Layoutlight";
-import imageOne from "./images/Group 60.png";
+
 
 import Convention from "./Convention";
 import Stadium from "./Stadium";
@@ -26,12 +20,6 @@ function NotableAchievement() {
         <Honour />
       </div>
     </LayoutLight>
-    // <>
-    //   notable
-    //   <div className="container">
-
-    //   </div>
-    // </>
   );
 }
 
