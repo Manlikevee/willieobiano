9b2703d5e1c6cf00cb1046eb42b477c4e6e6b31c
manlikevee <odahviktor@gmail.com>
2023-04-03 17:29:37 +0100

    previousPosition

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `willieobiano`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.willieobiano.com`
   },
   plugins: [{
     resolve: 'gatsby-plugin-google-analytics',
